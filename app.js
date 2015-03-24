bd8f087e0ceb8fe75d8a803bbd266b8f812a9326
Rosen <rosen_genius98@abv.bg>
2015-03-24 17:30:44 +0200

    Added ProductDetailsSection

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var app = angular.module('myStore', ['ngRoute']);
 
 app.config(function($routeProvider){
     $routeProvider
-
     .when('/', {
         templateUrl: "/views/productsSection.html",
         controller: 'ProductsSectionController'
+    })
+    .when('/product/:id', {
+    	templateUrl: "/views/productDetailsSection.html",
+    	controller: 'ProductDetailsSectionController'
     });
 });
--- controllers/productDetailsSection.js
+++ controllers/productDetailsSection.js
@@ -1,4 +1,4 @@
-app.controller('ProductsSectionController', function($scope, $http) {
+app.controller('ProductDetailsSectionController', function($scope, $http) {
     $http.get('http://localhost:8080/data/products.json').then(function(response){
         if(response.status == 200){  
             $scope.products = response.data;
