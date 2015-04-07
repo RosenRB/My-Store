a23a87ca0b1700ebbafa277b95c63c16c193a035
Marieta Ivanova <mivanova@obecto.com>
2015-04-07 17:26:30 +0300

    ProductDetailsSection completed and styles added

--- controllers/productDetailsSection.js
+++ controllers/productDetailsSection.js
@@ -1,4 +1,12 @@
-app.controller('ProductDetailsSectionController', function($scope, $routeParams) {
-    $scope.pesho = $routeParams.alias;
-
+app.controller('ProductDetailsSectionController', function($scope, $routeParams, $http) {
+    var alias = $routeParams.alias;
+    
+    $http.get('http://localhost:8080/data/' + alias + '.json').then(function(response){
+        if(response.status == 200){
+            $scope.details = response.data;
+        }else{
+            alert('Showing details error');
+        }
+    })
+    
 });
