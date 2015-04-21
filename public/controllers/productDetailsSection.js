f99953f885ead7162ec4c9c2084c10e4484c3d94
Marieta Ivanova <mivanova@obecto.com>
2015-04-21 17:34:30 +0300

    Session added

--- public/controllers/productDetailsSection.js
+++ public/controllers/productDetailsSection.js
@@ -7,6 +7,16 @@ app.controller('ProductDetailsSectionController', function($scope, $routeParams,
         }else{
             alert('Showing details error');
         }
-    })
+    });
     
+    $scope.onAddToCartButtonClick = function (){
+    	$http.post('http://localhost:8080/rest/cart/add', $scope.details).then(function(response){
+	        if(response.status == 200){
+	            //$scope.details = response.data;
+	            alert(response.data);
+	        }else{
+	            alert('Showing cart error');
+	        }
+    	});
+    }
 });
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
- 
-/*app.get('/', function (req, res) {
-  res.send('Hello World');
-});*/
+
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+app.use(bodyParser.json());
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false}));
+
+app.post('/rest/cart/add', function (req, res) {
+	//var msg = req.body.msg;
+	var sess = req.session;
+	if(sess.views){
+		sess.views++;
+	}else{
+		sess.views = 1;
+	}
+	//console.log(req.body);
+	console.log(sess.views);
+  	res.send(sess.views);
+});
 
 app.use(express.static('public'));
 
